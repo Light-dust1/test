60ad8acc47bbc0ef0504cb1e3e499a595fc0206a
Light <2753879382@qq.com>
2024-06-20 13:15:49 +0800

    完成后端登录注册功能（包括数据库）

---
+++ WorldofZuul/src/main/java/com/worldofzuul/pojo/Game.java
@@ -0,0 +1,90 @@
+/**
+ * 该类是“World-of-Zuul”应用程序的主类。
+ * 《World of Zuul》是一款简单的文本冒险游戏。用户可以在一些房间组成的迷宫中探险。
+ * 你们可以通过扩展该游戏的功能使它更有趣!.
+ *
+ * 如果想开始执行这个游戏，用户需要创建Game类的一个实例并调用“play”方法。
+ *
+ * Game类的实例将创建并初始化所有其他类:它创建所有房间，并将它们连接成迷宫；它创建解析器
+ * 接收用户输入，并将用户输入转换成命令后开始运行游戏。
+ *
+ * @author  Michael Kölling and David J. Barnes
+ * @version 1.0
+ */
+package com.worldofzuul.pojo;
+
+public class Game
+{
+    private Parser parser;
+    private Room currentRoom;     //当前房间
+
+    public Game()
+    {
+        createRooms();
+        parser = new Parser();
+    }
+
+    private void createRooms()
+    {
+        Room outside, theater, pub, lab, office;
+
+        // create the rooms
+        outside = new Room("outside the main entrance of the university");
+        theater = new Room("in a lecture theater");
+        pub = new Room("in the campus pub");
+        lab = new Room("in a computing lab");
+        office = new Room("in the computing admin office");
+
+        // 初始化出口（起点为outside），构建地图
+        outside.setExit("east", theater);
+        outside.setExit("south", lab);
+        outside.setExit("west", pub);
+
+        theater.setExit("west", outside);
+
+        pub.setExit("east", outside);
+
+        lab.setExit("north", outside);
+        lab.setExit("east", office);
+
+        office.setExit("west", lab);
+
+        currentRoom = outside;  // start game outside
+    }
+
+    public void play()
+    {
+        printWelcome();
+
+        // 进入主命令循环，反复读取命令并执行，直到游戏结束
+        boolean finished = false;
+        while (! finished) {
+            Command command = parser.getCommand();
+            if(command == null) {
+                System.out.println("I don't understand...");
+            } else {
+                finished = command.execute(this);
+            }
+        }
+
+        System.out.println("Thank you for playing.  Good bye.");
+    }
+
+    private void printWelcome()
+    {
+        System.out.println();
+        System.out.println("Welcome to the World of Zuul!");
+        System.out.println("World of Zuul is a new, incredibly boring adventure game.");
+        System.out.println("Type 'help' if you need help.");
+        System.out.println();
+        System.out.println(currentRoom.getLongDescription());
+    }
+
+    public Room getCurrentRoom() {
+        return currentRoom;
+    }
+
+    public void setCurrentRoom(Room room){
+        this.currentRoom = room;
+    }
+}
---
+++ WorldofZuul/src/test/java/com/example/worldofzuul/WorldofZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.worldofzuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorldofZuulApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
